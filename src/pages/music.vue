c369decfa679ac9da55fea948cc04894660a5db8
zhangjialin <jialin2013@qq.com>
2017-05-17 09:33:16 +0800

    5

--- src/pages/music.vue
+++ src/pages/music.vue
@@ -1,65 +1,48 @@
 <style>
-    @import '../styles/page/video.css';
+    @import '../styles/page/music.css';
 </style>
 <template>
-    <div id="video-page" :style="{ 'min-height': (global.winHeight - 60)+'px' }">
-        <div class="wrap blue">
-            <div class="content clear-fix">
-                <h2>近期最热</h2>
-                <div class="item">
-                    <img src="/static/video/1-1.jpg"/>
-                    <div>
-                        <img src="/static/video/1.jpg"/>
-                        <div>星际争霸对抗<span>游戏竞技</span></div>
-                        <div>隐约的吃货</div>
-                    </div>
-                </div>
-                <div class="item">
-                    <img src="/static/video/1-2.jpg"/>
-                    <div>
-                        <img src="/static/video/2.jpg"/>
-                        <div>帝国时代2<span>热门推荐</span></div>
-                        <div>马超</div>
-                    </div>
-                </div>
-                <div class="item">
-                    <img src="/static/video/1-3.jpg"/>
-                    <div>
-                        <img src="/static/video/3.jpg"/>
-                        <div>穿越火线<span>游戏竞技</span></div>
-                        <div>月亮之上</div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="wrap pad">
-            <div class="content clear-fix">
-                <h2>游戏解说</h2>
-                <div class="item">
-                    <img src="/static/video/2-1.jpg"/>
-                    <div>
-                        <img src="/static/video/4.jpg"/>
-                        <div>魔兽争霸<span>游戏竞技</span></div>
-                        <div>吃遍中国</div>
-                    </div>
-                </div>
-                <div class="item">
-                    <img src="/static/video/2-2.jpg"/>
-                    <div>
-                        <img src="/static/video/5.jpg"/>
-                        <div>英雄联盟精英赛<span>游戏竞技</span></div>
-                        <div>干了这杯酒</div>
-                    </div>
-                </div>
-                <div class="item">
-                    <img src="/static/video/2-3.jpg"/>
-                    <div>
-                        <img src="/static/video/6.jpg"/>
-                        <div>王者荣耀<span>游戏竞技</span></div>
-                        <div>我爱着你</div>
-                    </div>
-                </div>
-            </div>
+    <div id="music-page" :style="{ 'min-height': (global.winHeight - 60)+'px' }">
+        <div class="wrap">
+            <h2>《红颜劫》</h2>
+            <p>斩断情丝心犹乱</p>
+            <p>千头万绪仍纠缠</p>
+            <p>拱手让江山</p>
+            <p>低眉恋红颜</p>
+            <p>祸福轮流转</p>
+            <p>是劫还是缘</p>
+            <p>天机算不尽</p>
+            <p>交织悲与欢</p>
+            <p>古今痴男女</p>
+            <p>谁能过情关</p>
+            <p>拱手让江山</p>
+            <p>低眉恋红颜</p>
+            <p>祸福轮流转</p>
+            <p>是劫还是缘</p>
+            <p>天机算不尽</p>
+            <p>交织悲与欢</p>
+            <p>古今痴男女</p>
+            <p>谁能过情关</p>
+            <p>谁能过情关</p>
+            <p>小山重叠金明灭</p>
+            <p>鬓云欲渡香腮雪</p>
+            <p>懒起画蛾眉</p>
+            <p>弄妆梳洗迟</p>
+            <p>照花前后镜</p>
+            <p>花面交相映</p>
+            <p>新帖绣罗襦</p>
+            <p>双双金鹧鸪</p>
+            <p>新帖绣罗襦</p>
+            <p>双双金鹧鸪</p>
+            <p>懒起画蛾眉</p>
+            <p>弄妆梳洗迟</p>
+            <p>照花前后镜</p>
+            <p>花面交相映</p>
+            <p>新帖绣罗襦</p>
+            <p>双双金鹧鸪</p>
+            <p>新帖绣罗襦</p>
+            <p>双双金鹧鸪</p>
+            <p>双双金鹧鸪</p>
         </div>
         <div class="footer-wrap">Copyright © 2017 中新石豆传媒技术有限公司<a href="http://www.miitbeian.gov.cn/" target="_blank">粤ICP备17041476号</a></div>
     </div>
@@ -72,8 +55,6 @@
             return {
                 global: Global
             }
-        },
-        mounted: function () {
         }
     }
 </script>
